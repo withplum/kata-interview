dbcea0a283e58b923d88255534ee975685551c6c
Ilias Mentzelos <iliasmentz@gmail.com>
2021-05-14 13:59:13 +0300

    Add unusual-spending-ts kata

---
+++ unusual-spending-ts/src/payments-api/index.ts
@@ -0,0 +1,19 @@
+interface Payment {
+  category: string;
+  amount: number;
+}
+
+const payments = [
+  {
+    category: 'some category name',
+    amount: 13.37
+  }
+]
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-ts-comment
+// @ts-ignore
+async function getPayments(userId: string): Promise<Payment[]> {
+  return payments
+}
+
+export default getPayments;
